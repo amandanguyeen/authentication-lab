27401a3a5c07d7472a0531c4e1e2e00bc2c17e50
Amanda Nguyen <amanda6@ualberta.ca>
2022-11-04 12:22:06 -0600

    completed lab 9

--- authlab/settings.py
+++ authlab/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'client.apps.ClientConfig',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -121,12 +122,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# Django Rest Framework
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
     )
 }
